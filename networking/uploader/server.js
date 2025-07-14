0e193916e93d9bd0ab504957ea99bbdd2cfc9315
Saeed ul Haq <saeedulhaquet@gmail.com>
2025-07-14 19:26:59 +0500

    revcieved the filename dynamically and store it in location

--- networking/uploader/server.js
+++ networking/uploader/server.js
@@ -3,22 +3,29 @@ const fs = require("node:fs/promises");
 
 const PORT = 5050;
 const HOST = "::1";
-const path = "./storage/test.txt";
 const server = net.createServer();
+let fileHandle, fileWriteStream;
 
 server.on("connection", async (socket) => {
   console.log("A new connection has been made");
-  const fileHandle = await fs.open(path, "w");
-  const fileWriteStream = fileHandle.createWriteStream();
 
   socket.on("data", async (data) => {
+    if (!fileHandle) {
+      socket.pause(); // pause receiving data from the client
+      const indexOfSeparator = data.indexOf("------");
+      const fileName = data.subarray(10, indexOfSeparator).toString("utf-8");
+      fileHandle = await fs.open(`storage/${fileName}`, "w");
+      fileWriteStream = fileHandle.createWriteStream(); // the stream to write to our file
+      fileWriteStream.write(data.subarray(indexOfSeparator + 6));
+      socket.resume();
+
+      fileWriteStream.on("drain", () => {
+        socket.resume();
+      });
+    }
     if (!fileWriteStream.write(data)) {
       socket.pause();
     }
-  });
-
-  fileWriteStream.on("drain", () => {
-    socket.resume();
   });
 
   const cleanup = () => {
@@ -26,11 +33,14 @@ server.on("connection", async (socket) => {
     console.log("Connection closed");
   };
 
-  socket.on("end", cleanup);
+  socket.on("end", () => {
+    fileHandle && fileHandle.close();
+    console.log("Connection closed");
+  });
+
   socket.on("error", cleanup);
-  fileWriteStream.on("error", cleanup);
 });
 
-server.listen(PORT, HOST, (add) => {
+server.listen(PORT, HOST, (address) => {
   console.log("Server is runnning on at", server.address());
 });
